f1992baa5bf69e8c1e4fb34e1f7f449a3a522a17
a10242 <a10242@alunos.ipca.pt>
2018-06-13 06:13:08 +0100

    Revert "ControllerModelViews"
    This reverts commit cdb28b9fe184da2df5c4be60f6473535d346957c.

---
+++ trabalho_base_dados/trabalho_base_dados/Views/MainMenu.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace trabalho_base_dados
+{
+    public partial class MainMenu : Form
+    {
+        private MainMenu MenuForm;
+        private Pessoal PessoalForm;
+
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pessoalBtn_Click(object sender, EventArgs e)
+        {
+
+            Pessoal pessoal = new Pessoal();
+            pessoal.ShowDialog();
+            pessoal.Show();
+            this.Close();
+
+        }
+
+        private void producaoBtn_Click(object sender, EventArgs e)
+        {
+            // this.Hide();
+            Producao vinho = new Producao();
+            vinho.Show();
+        }
+        private void armazemBtn_Click(object sender, EventArgs e)
+        {
+            Armazem armazem = new Armazem();
+            armazem.Show();
+            this.Close();
+        }
+        private void marketBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ trabalho_base_dados/trabalho_base_dados/Views/Pessoal.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace trabalho_base_dados
+{
+    public partial class Pessoal : Form
+    {
+        public Pessoal()
+        {
+            InitializeComponent();
+        }
+
+        private void Clientes_Load(object sender, EventArgs e)
+        {
+            // TODO: esta linha de código carrega dados na tabela 'nelsadaDataSet.Cliente'. Você pode movê-la ou removê-la conforme necessário.
+            //this.clienteTableAdapter.Fill(this.nelsadaDataSet.Cliente);
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
